b98332c79ba0e4fda1c950844a0643a8e22dc906
Sowmya-5477 <25kq1a5477@pace.ac.in>
2025-11-26 20:58:39 +0530

    Add files via upload

---
+++ even or odd using function.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+void num();
+num();
+printf("\nEnd of the program");
+}
+void num()
+{
+ int n;
+ printf("Enter n value:\n");
+ scanf("%d",&n);
+ if(n%2==0)
+ {
+  printf("Even number");
+ } 
+ else
+ {
+  printf("Odd number");
+ } 
+ } 
+ 
